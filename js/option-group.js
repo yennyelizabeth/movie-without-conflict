a8b8afcd65bd0c73118870316d5b3f3d982337b2
yennyelizabeth <yenny.elizabeth@hotmail.com>
2018-01-18 18:13:00 -0500

    añadiendo funcionalidad a vista option-group

--- js/option-group.js
+++ js/option-group.js
@@ -1,27 +1,65 @@
 /* Funcionalidad Vista Opción Ver Película en Grupo */
 $(document).ready(function() {
-  /* elementos de la base de datos*/
+  /*Arreglos con Preferencias existentes en la API*/
   var musical= ['Cannibal! The Musical','Lovestruck: The Musical','College Musical','Cats'];
-  var fantasy = ['Lovestruck: The Musical', 'Shrek the Musical', ]
+  var fantasy = ['Lovestruck: The Musical', 'Shrek the Musical']
+  var ficcion = ['alien', 'batman', 'lego','mascara','Cannibal! The Musical'];  
+  var accion = ['Star Wars', 'Wonder Woman', 'Scott Pilgrim vs. the World', 'Tropic Thunder', 'Pirates of the Caribbean', 'Shaolin Soccer', 'Airbag'];
+  var animada = ['Up', 'Moana'];
+  var aventura = ['Star Wars', 'Up','Moana','Pirates of the Caribbean', 'Back to the Future', 'Wonder Woman'];
+  var cienciaficcion = ['The Adjustment Bureau', 'Back to the Future'];
+  var comedia = ['Up','Moana', 'Scott Pilgrim vs. the World','Tropic Thunder', 'Shaolin Soccer', 'Airbag', 'Back to the Future'];
+  var drama = ['Night and Day'];
+  var erotico = [];
+  var romantica = ['Possession ',' Date Movie, Love Actually','Sun, Sand & Romance','Modern Romance'];
+  var suspenso = ['The Sixth Sense ',' Vertigo', 'Psycho','Hitchcock', 'Memento'];
+  var terror = ['Raising Cain',' American Nightmare', 'The grudge','Grave Halloween', 'The others'];
 
   /**/
   var array1=[];
 
   /* eventos para cada tipo de genero de la pelicula*/
-  $('#ciencia').click(function(){
-    var newArray= '';
-    array2 = musical
-    array3= fantasy
-    array1= $.merge(array2, array3);
-    console.log(array1)
+  var total=[];
+  var flag = 0;
+  
+  $('#musical').click(function(){	
+    flag = 1;
+    alert(flag);
+    total=$.merge(total,musical);
+    alert(total);
   })
-
-  $('#romantica').click(function(){
-    var newArray= '';
-    array2 = musical
-    array3= fantasy
-    array1= $.merge(array2, array3);
+  
+  $('#fantasy').click(function(){
+     flag = 2;
+     alert(flag);
+     total=$.merge(total,fantasy);
+     alert(total);
   })
+  
+  $('#ficcion').click(function(){
+    flag = 3 ;
+    total=$.merge(total,ficcion);
+    alert(total);  
+    })
+  
+  $('#accion').click(function(){
+    flag = 4 ;
+    total=$.merge(total,accion);
+    alert(total);  
+  })
+  
+  $('#animada').click(function(){
+    flag = 5 ;
+    total=$.merge(total,accion);
+    alert(total);  
+  })
+  
+  $('#aventura').click(function(){
+    flag = 6 ;
+    total=$.merge(total,aventura);
+    alert(total);  
+  })
+  
 
   /* funcion para llamar el api*/
   function apiCall(){
